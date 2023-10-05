aec7854cfc464333dbbed27d1667956ffce7b54f
Iamneo <techadmin@iamneo.ai>
2023-10-05 16:46:06 +0000

    Backup Commit

--- dotnetproject/dotnetmicroserviceone/Controllers/EmployeeController.cs
+++ dotnetproject/dotnetmicroserviceone/Controllers/EmployeeController.cs
@@ -8,5 +8,21 @@ using System.Threading.Tasks;
 [ApiController]
 public class EmployeeController:ControllerBase
 {
-    private readonly EmployeeDbContext
+    private readonly EmployeeDbContext_context;
+
+    public EmployeeController(EmployeeDbContext context)
+    {
+        _context =context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IOrderedEnumerable<Employee>>>
+    GetAllEmployees()
+    {
+        return await_context.Employee.ToListAsync();
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Employee>>GetEmployeeById(int id)
+    {
+        var employee=await_context.F
+    }
 }
